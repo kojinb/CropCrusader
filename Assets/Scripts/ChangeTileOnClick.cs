4fb88333061eba11d88450534b6704179c3046ef
missyfang <57602734+missyfang@users.noreply.github.com>
2023-04-25 16:06:57 -0400

    Set up tile selection system.
    Created an accessible and inaccessible tile map.
    Create script to lighten inaccessible tiles on hover and make the accessible on left click.

---
+++ Assets/Scripts/ChangeTileOnClick.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ChangeTileOnClick : MonoBehaviour
+{
+    [SerializeField]
+    Tile accessibleTile;
+    [SerializeField]
+    Tile inaccessibleTile;
+    [SerializeField]
+    Tile hoverTile;
+    [SerializeField]
+    Tilemap accessibleMap;
+    [SerializeField]
+    Tilemap inaccessibleMap;
+
+    private Vector3Int previousMousePos = new Vector3Int(); 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3Int mousePos = GetMousePosition();
+
+        // Reset previous hovered over tile
+        if (!mousePos.Equals(previousMousePos) && inaccessibleMap.HasTile(previousMousePos))
+            inaccessibleMap.SetTile(previousMousePos, inaccessibleTile);
+
+        if (!mousePos.Equals(previousMousePos) && inaccessibleMap.HasTile(mousePos))
+        {
+            // Highlight hovered over tile
+            inaccessibleMap.SetTile(mousePos, null);
+            inaccessibleMap.SetTile(mousePos, hoverTile);
+            previousMousePos = mousePos;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            inaccessibleMap.SetTile(mousePos, null);
+            accessibleMap.SetTile(mousePos, accessibleTile);
+        }
+    }
+
+    Vector3Int GetMousePosition()
+    {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return inaccessibleMap.WorldToCell(mouseWorldPos);
+    }
+}
